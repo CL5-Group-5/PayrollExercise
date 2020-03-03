69b91499c2b49fa05d7792d21390d304be831ed8
NoeRiveraJr <noe.rivera.jr@outlook.com>
2020-03-02 21:12:08 -0500

    Added the necessary arrays for the exercise

---
+++ PayrollExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] empId = { 56588, 45012, 78951, 87775, 84512, 13028, 75804 };
+            int[] hours = new int[7];
+            double[] payRate = new double[7];
+            double[] wages = new double[7];
+
+        }
+    }
+}
