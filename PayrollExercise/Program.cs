e8586bae019c2c12500dce951fa77b238f0ea4c3
NoeRiveraJr <noe.rivera.jr@outlook.com>
2020-03-12 13:45:10 -0400

    Removed the payRoll functions class because it is unnecessary for this program

--- PayrollExercise/Program.cs
+++ PayrollExercise/Program.cs
@@ -10,9 +10,9 @@ namespace PayrollExercise
     {
         static void Main(string[] args)
         {
-            int[] empId = { 56588, 45012, 78951, 87775, 84512, 13028, 75804 };
-            int[] hours = new int[7];
-            double[] payRate = new double[7];
+            int[] employeeId = { 56588, 45012, 78951, 87775, 84512, 13028, 75804 };     //created 4 arrays to simulate storage containers for an employee's
+            int[] hours = new int[7];                                                   //wages, each index in every array is attached to a specfic employee
+            double[] payRate = new double[7];                                           //by the employeeId array
             double[] wages = new double[7];
 
         }
