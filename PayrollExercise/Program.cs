573142f09cbd79505513785be4222ad152b1d7aa
Teqwon <59837818+Teqwon@users.noreply.github.com>
2020-03-12 14:58:48 -0400

    Created a for loop that will iterate through all the arrays and got them to output the correct pay for each individidual employee ID

--- PayrollExercise/Program.cs
+++ PayrollExercise/Program.cs
@@ -10,14 +10,31 @@ namespace PayrollExercise
     {
         static void Main(string[] args)
         {
+            int hh;
+            double pay;
+      
             int[] employeeId = { 56588, 45012, 78951, 87775, 84512, 13028, 75804 };     //created 4 arrays to simulate storage containers for an employee's
             int[] hours = new int[7];                                                   //wages, each index in every array is attached to a specfic employee
             double[] payRate = new double[7];                                           //by the employeeId array
             double[] wages = new double[7];
 
             Console.WriteLine("You are an employee that calculates weekly wages for all the employee's at your company.\n" +
-            "This program was designed to assist you with calculating this wages!\nYou will entering in the amount of hours" +
-            "and the hourly payRate for 7 employees.");
+            "This program was designed to assist you with calculating their wages!\nYou will be entering in the amount of hours\n" +
+            "and the amount they get paid for one week worth of pay for 7 employees.\n\n");
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Enter the amount of hours {employeeId[i]} has worked this week");
+                hh=int.Parse( Console.ReadLine());
+                Console.WriteLine($"Enter the how much {employeeId[i]} makes per hour");
+                pay= double.Parse( Console.ReadLine());
+                Console.WriteLine($"{employeeId[i]} made ${wages[i] = (hh * pay)} this week.");
+                Console.ReadLine();
+                Console.Clear();
+
+
+            }
+
 
 
         }
