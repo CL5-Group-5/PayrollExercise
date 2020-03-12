a1aec1420ce1c7e34edd2fb2654cd00de19287be
NoeRiveraJr <noe.rivera.jr@outlook.com>
2020-03-12 13:50:41 -0400

    Added intro to the program

--- PayrollExercise/Program.cs
+++ PayrollExercise/Program.cs
@@ -14,7 +14,11 @@ namespace PayrollExercise
             int[] hours = new int[7];                                                   //wages, each index in every array is attached to a specfic employee
             double[] payRate = new double[7];                                           //by the employeeId array
             double[] wages = new double[7];
-            //todo
+
+            Console.WriteLine("You are an employee that calculates weekly wages for all the employee's at your company.\n" +
+            "This program was designed to assist you with calculating this wages!\nYou will entering in the amount of hours" +
+            "and the hourly payRate for 7 employees.");
+
 
         }
     }
